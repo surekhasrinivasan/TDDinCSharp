81d520dd4316a2f9cd4708ed6448b2ec121409b1
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-27 16:37:08 -0400

    Test Driven Developmnet in C# usingVSTS unit Testing

--- MathsTestProject/UnitTest1.cs
+++ MathsTestProject/UnitTest1.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathsComponentNameSpace;
 
 namespace MathsTestProject
 {
     [TestClass]
     public class UnitTest1
     {
+        //Testcase todo Addition
         [TestMethod]
         public void TestMethod1()
         {
+            MathsComponent obj = new MathsComponent();
+            int result = obj.Add(10, 10);
+            Assert.AreEqual(20, result);
         }
     }
 }
